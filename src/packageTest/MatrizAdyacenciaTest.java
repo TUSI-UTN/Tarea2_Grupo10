8e87133cdd9ed64da6b6125dcc69b9ac5a08d599
Tomas Scutti <tomas.scutti@gmail.com>
2023-04-12 22:54:24 -0300

    tscutti | setUp method was deleted, now the matriz is inicializated on every test

--- src/packageTest/MatrizAdyacenciaTest.java
+++ src/packageTest/MatrizAdyacenciaTest.java
@@ -1,6 +1,5 @@
 package packageTest;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import frgp.utn.edu.ar.main.MatrizAdyacencia;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,39 +8,44 @@ public class MatrizAdyacenciaTest {
 	
 	private MatrizAdyacencia matriz;
 	
-	@BeforeEach
-    public void setUp() throws Exception {
-        matriz = new MatrizAdyacencia(5);
-    }
-
     @Test
     public void agregarElementoTest() {
-    	matriz.agregarElemento(0, 1); 
-        assertTrue(matriz.existeElemento(0, 1)); 
+    	matriz = new MatrizAdyacencia(1);
+    	matriz.agregarElemento(0, 0); 
+        assertTrue(matriz.existeElemento(0, 0)); 
     }
     
     @Test
     public void agregarElementoSimetriaTest() {
-        matriz.agregarElemento(2, 3);
-        assertTrue(matriz.existeElemento(2, 3));
-        assertTrue(matriz.existeElemento(3, 2));
+    	matriz = new MatrizAdyacencia(2);
+        matriz.agregarElemento(0, 1);
+        assertTrue(matriz.existeElemento(0, 1));
+        assertTrue(matriz.existeElemento(1, 0));
     }
     
     
     @Test
     public void eliminarElementoTest() {
-    	matriz.eliminarElemento(2, 3);
-    	assertTrue(!matriz.existeElemento(2, 3));
+    	matriz = new MatrizAdyacencia(1);
+    	matriz.eliminarElemento(0, 0);
+    	assertTrue(!matriz.existeElemento(0, 0));
     }
-    
     
     @Test
     public void eliminarElementoSimetricoTest() {
-        
+    	matriz = new MatrizAdyacencia(2);
+    	matriz.agregarElemento(0, 1);
+    	matriz.eliminarElemento(1, 0);
+    	assertTrue(!matriz.existeElemento(0, 1));
     }
     
     @Test
     public void contarRelacionesTest() {
-        
+    	matriz = new MatrizAdyacencia(4);
+    	matriz.agregarElemento(0, 1);
+    	matriz.agregarElemento(1, 2);
+    	matriz.agregarElemento(2, 3);
+    	
+    	assertTrue(matriz.getCantidadElementos() == 3);
     }
 }
