873d6251b085619d349634e739c1014d418d529f
Ledesma Marcos <ledesma.mn3@gmail.com>
2023-04-15 22:39:27 -0300

    Junit class & package test

---
+++ src/packageTest2/AllClassTest.java
@@ -0,0 +1,15 @@
+package packageTest2;
+
+import org.junit.runner.RunWith;
+
+import packageTest.MatrizAdyacenciaTest;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses(MatrizAdyacenciaTest.class)
+public class AllClassTest {
+
+}
